97594b9fdc214104146c916f88b344c544b89b1a
반선호 <vsh123@naver.com>
2022-08-22 17:56:49 +0900

    feat: 서브모듈 배포 스크립트 추가

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ cp ./auth/application-prod.yml ./src/main/resources/
 
 ./gradlew clean appNpmBuild build
 
-nohup java -jar --Dspring.active.profiles=prod build/libs/kurly-0.0.1-SNAPSHOT.jar &
+nohup java -jar -Dspring.profiles.active=prod build/libs/kurly-0.0.1-SNAPSHOT.jar &
