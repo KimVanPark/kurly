6b40c3981973e9c8429ad4010bedf6a4eff0cdac
반선호 <vsh123@naver.com>
2022-08-22 23:51:37 +0900

    feat: lombok plugin 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
+    kotlin("plugin.lombok") version "1.6.21"
 }
 
 allOpen {
